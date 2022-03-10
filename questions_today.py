0efbd3772a0cc9d26c881885e582553e925d3def
Joel Negrao <joelbisponeto@gmail.com>
2022-03-10 17:36:49 +0100

    Adding two more questions

--- questions_today.py
+++ questions_today.py
@@ -8,7 +8,7 @@ already_answered = [
     41, 27,
     6, 14, 66, 57, 56, 
     4, 26, 53,
-    76, 39, 9
+    76, 39, 9,55, 65
 ]
 
 print(choice([i for i in range(2, 77) if i not in already_answered]))
