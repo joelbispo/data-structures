ce37a5f98f44c6761055aca20586b02100fe3707
Joel Negrao <joelbisponeto@gmail.com>
2022-03-10 16:41:05 +0100

    Adding two more questions

---
+++ leetcode/217_contains_dupllicate.py
@@ -0,0 +1,12 @@
+from operator import truediv
+
+
+class Soluion:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
--- questions_today.py
+++ questions_today.py
@@ -6,7 +6,9 @@ already_answered = [
     43, 54, 51,
     17,
     41, 27,
-    6, 14, 66, 57, 56
+    6, 14, 66, 57, 56, 
+    4, 26, 53,
+    76, 39, 9
 ]
 
 print(choice([i for i in range(2, 77) if i not in already_answered]))
