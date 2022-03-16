0dfa8858e0debd9631db13f83b7026f181c9bf85
Joel Negrao <joelbisponeto@gmail.com>
2022-03-16 06:40:15 +0100

    Adding three  more questions

--- questions_today.py
+++ questions_today.py
@@ -5,15 +5,25 @@ already_answered = [
     25, 49,
     43, 54, 51,
     17,
-    41, 27,
+    41, 27, 23,
     6, 14, 66, 57, 56, 
     4, 26, 53,
     76, 39, 9,55, 65,
     42, 15, 68, 73, 38, 20,
-    52, 7, 31, 24,  2, 13
+    52, 7, 31, 24,  2, 13,
+    70, 11, 60, 69, 46, 22,
+    21, 12, 58, 5, 29, 3,
+    50, 72, 34, 48, 16, 61
 
+]
+
+already_reviewed = [
+    27, 
 ]
 
 print(len(already_answered))
 
-#print(choice([i for i in range(2, 77) if i not in already_answered]))
+#print(choice([i for i in already_answered]))
+
+
+# print(choice([i for i in range(2, 77) if i not in already_answered]))
