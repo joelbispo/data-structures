64b2a4cdb4c850f1ed5bd3f1e78b2f95d1d823b7
Joel Negrao <joelbisponeto@gmail.com>
2022-05-22 11:26:15 +0200

    question 746 and 53

--- leetcode/53_max_continuous_subarray_sum.py
+++ leetcode/53_max_continuous_subarray_sum.py
@@ -1,4 +1,5 @@
 class Solution:
+    #solution 1
     def maxContiguousSubarraySum(self, nums):
         max_so_far = nums[0]
         max_ending_here = nums[0]
@@ -9,3 +10,16 @@ class Solution:
             max_so_far = max(max_ending_here, max_so_far)
 
         return max_so_far
+
+    #solution 2
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+        
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        
+        return maxSub
